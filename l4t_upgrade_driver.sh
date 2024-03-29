92513858659b1cdf1c2607141b4395ff3dd42913
Manx98 <wenyiyu98@gmail.com>
2024-03-30 01:09:09 +0800

    添加描述

--- l4t_upgrade_driver.sh
+++ l4t_upgrade_driver.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "Installing jetson-stats..."
 sudo pip3 install -U jetson-stats
 if [ $? -ne 0 ]; then
     echo "Failed to install jetson-stats."
     exit $?
 fi
+echo "Overwriting /etc/apt/preferences.d/00-switch-bsp-restrictions"
 sudo cat << EOF > /etc/apt/preferences.d/00-switch-bsp-restrictions
 # Disallow L4T 32.4.x
 Package: nvidia-l4t-*
@@ -29,6 +31,7 @@ if [ $? -eq 0 ]; then
     echo "Failed to overwrite /etc/apt/sources.list.d/nvidia-l4t-apt-source.list"
     exit $?
 fi
+echo "Overwrite /etc/apt/sources.list.d/nvidia-l4t-apt-source.list"
 cat << EOF > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 deb https://repo.download.nvidia.com/jetson/common r32.7 main
 deb https://repo.download.nvidia.com/jetson/t210 r32.7 main
@@ -37,21 +40,25 @@ if [ $? -eq 0 ]; then
     echo "Failed to overwrite /etc/apt/sources.list.d/nvidia-l4t-apt-source.list"
     exit $?
 fi
+echo "touch /etc/nv_boot_control.conf"
 sudo touch /etc/nv_boot_control.conf
 if [ $? -eq 0 ]; then
     echo "Failed to touch /etc/nv_boot_control.conf"
     exit $?
 fi
+echo "mkdir /opt/nvidia/l4t-packages"
 sudo mkdir /opt/nvidia/l4t-packages
 if [ $? -eq 0 ]; then
     echo "Failed to mkdir /opt/nvidia/l4t-packages"
     exit $?
 fi
+echo "touch /opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall"
 sudo touch /opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall
 if [ $? -eq 0 ]; then
     echo "Failed to touch /opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall"
     exit $?
 fi
+echo "Update and Upgrade..."
 sudo apt update
 if [ $? -eq 0 ]; then
     echo "Failed to apt update"
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+echo "Change apt source to mirrors.tuna.tsinghua.edu.cn."
 sudo sed -i 's/ports.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
 if [ $? -ne 0 ]; then
     echo "Failed to change apt source."
     exit $?
 fi
+echo "Update apt source list."
 sudo apt update
 if [ $? -ne 0 ]; then
     echo "Failed to update apt source list."
     exit $?
 fi
+echo "Upgrade packages."
 sudo apt upgrade -y
 if [ $? -ne 0 ]; then
     echo "Failed to upgrade packages."
     exit $?
 fi
+echo "Install software."
 sudo apt install -y git zsh curl wget vim cmake gcc python3-pip
 if [ $? -ne 0 ]; then
     echo "Failed to install software."
     exit $?
 fi
+echo "Setup current user git config."
 mkdir -p ~/.pip && cat << EOF > ~/.pip/pip.conf
 [global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
@@ -29,6 +34,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to setup current user pip3 config."
     exit $?
 fi
+echo "Setup root user git config."
 sudo mkdir -p ~/.pip && sudo cat << EOF > ~/.pip/pip.conf
 [global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
@@ -39,26 +45,31 @@ if [ $? -ne 0 ]; then
     echo "Failed to setup root user pip3 config."
     exit $?
 fi
+echo "Download oh-my-zsh install script."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 if [ $? -ne 0 ]; then
     echo "Failed to download oh-my-zsh install script."
     exit $?
 fi
+echo "Clone zsh-syntax-highlighting."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 if [ $? -ne 0 ]; then
     echo "Failed to clone zsh-syntax-highlighting."
     exit $?
 fi
+echo "Clone zsh-autosuggestions."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 if [ $? -ne 0 ]; then
     echo "Failed to clone zsh-autosuggestions."
     exit $?
 fi
+echo "Setup zsh theme."
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
 if [ $? -ne 0 ]; then
     echo "Failed to setup zsh theme."
     exit $?
 fi
+echo "Setup zsh plugins."
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
 if [ $? -ne 0 ]; then
     echo "Failed to setup zsh plugins."
